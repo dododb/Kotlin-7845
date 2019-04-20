40bfc8323625748c5f46d0eb036211385a5b691a
dorian bataille <dorian.bataille@viacesi.fr>
2019-04-20 15:58:35 -0500

    Add files via upload

---
+++ labs/lab3/src/main/kotlin/meta1.kt
@@ -0,0 +1,24 @@
+import kotlin.reflect.*
+import kotlin.reflect.full.primaryConstructor
+
+fun <T: Any>strinit(klass: KClass<T>, data: String) : T?
+{
+    val args = data.split(",")
+    val argsArray = args.toTypedArray()
+
+    val c = klass.primaryConstructor
+    val instance = c?.call(*argsArray)
+    return instance
+}
+
+public class Personne(val nom: String, val prenom: String)
+{
+    override fun toString(): String {
+        return "$nom $prenom"
+    }
+}
+
+public fun main(args: Array<String>) {
+    val p = strinit(Personne::class, "jon,Snow")
+    println(p)
+}
